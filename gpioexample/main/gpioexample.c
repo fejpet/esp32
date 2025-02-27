09c722552d3d5eb0538f4d559cc874664e88545c
fejpet <fejp3t@gmail.com>
2025-02-27 20:23:00 +0100

    initial program

---
+++ gpioexample/main/gpioexample.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    gpio_set_direction(GPIO_NUM_22, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(GPIO_NUM_22, GPIO_PULLUP_ONLY);
+
+    gpio_set_direction(GPIO_NUM_21, GPIO_MODE_OUTPUT);
+    bool relayOn = false;
+    while (true)
+    {
+        if (relayOn)
+        {
+            gpio_set_level(GPIO_NUM_21, 0);
+        }
+        else
+        {
+            gpio_set_level(GPIO_NUM_21, 1);
+        }
+        relayOn = !relayOn;
+
+        vTaskDelay(1000);
+    }
+}
